66c386289b54ddfa7183ff0e7bd089b9eec20d93
Mohit Vasnani <mohitvasnani003@gmail.com>
2026-03-28 17:17:41 +0000

    initial portfolio

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,26 +2,33 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
-
+import Xicon from '@mui/icons-material/X';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThreads, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 function Main() {
 
   return (
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src={process.env.PUBLIC_URL + "/dp.jpg"} alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/Mohitvasnani" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/mohit-vasnani-6b11041b8/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://x.com/MohitVasnani47" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faXTwitter} /></a>
+            <a href="https://www.threads.com/@whatnowdrpgn" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faThreads} /></a>
+            
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          <h1>Mohit Vasnani</h1>
+          <p>Full Stack Web Developer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/Mohitvasnani" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/mohit-vasnani-6b11041b8/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://x.com/MohitVasnani47" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faXTwitter} /></a>
+            <a href="https://www.threads.com/@whatnowdrpgn" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faThreads} /></a>
           </div>
         </div>
       </div>
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,67 +1,57 @@
 import React from "react";
-import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { faBriefcase, faGraduationCap } from '@fortawesome/free-solid-svg-icons';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import '../assets/styles/Timeline.scss'
+import '../assets/styles/Timeline.scss';
 
 function Timeline() {
   return (
     <div id="history">
       <div className="items-container">
-        <h1>Career History</h1>
+        <h1>Career & Education</h1>
         <VerticalTimeline>
+
+          {/* MSc */}
           <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
-            contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - present"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
+            date="2025 – Present"
+            iconStyle={{ background: '#5000ca', color: '#fff' }}
+            icon={<FontAwesomeIcon icon={faGraduationCap} />}
           >
-            <h3 className="vertical-timeline-element-title">Technology Consultant</h3>
-            <h4 className="vertical-timeline-element-subtitle">Dallas, TX</h4>
+            <h3>MSc Advanced Computer Science</h3>
+            <h4>University of Liverpool, UK</h4>
             <p>
-              Full-stack Web Development, GenAI/LLM, Project Management, Business Development
+              Focused on software engineering, data systems, and real-world problem solving.
             </p>
           </VerticalTimelineElement>
+
+          {/* Internship */}
           <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2020 - 2022"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            date="Feb 2024 – July 2024"
+            iconStyle={{ background: '#5000ca', color: '#fff' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Full Stack Engineer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3>Graduate Software Developer Intern (Full-Stack)</h3>
+            <h4>NETPARAM Technologies, India</h4>
             <p>
-              Frontend Development, Backend Development, User Experience, Team Leading
+              Worked on Dealkarts (e-commerce platform). Built reusable React components,
+              implemented authentication, integrated APIs using Axios, and collaborated using Git/GitHub.
             </p>
           </VerticalTimelineElement>
+
+          {/* BTech */}
           <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2021 - 2021"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
+            date="2020 – 2024"
+            iconStyle={{ background: '#5000ca', color: '#fff' }}
+            icon={<FontAwesomeIcon icon={faGraduationCap} />}
           >
-            <h3 className="vertical-timeline-element-title">Staff Engineer Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3>BTech Computer Science</h3>
+            <h4>JECRC University, Jaipur</h4>
             <p>
-              Full-stack Development, API Development, User Experience
+              GPA: 7.58. Built strong foundations in programming, data structures, and software development.
             </p>
           </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2020 - 2020"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
-          >
-            <h3 className="vertical-timeline-element-title">Data Analyst Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Tokyo, Japan</h4>
-            <p>
-              Automation, Data Governance, Statistical Analysis
-            </p>
-          </VerticalTimelineElement>
+
         </VerticalTimeline>
       </div>
     </div>
